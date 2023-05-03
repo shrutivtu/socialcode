04f9f2e29e1a483cfd513b9007c4ba6c5c69c355
shrutivtu <shrutisaxena1596@gmail.com>
2023-05-03 10:06:23 +0530

    Feat: style modification

--- src/app/github/github.tsx
+++ src/app/github/github.tsx
@@ -17,7 +17,6 @@ const Github: FC = () => {
     });
 
     async function getTest() {
-        console.log(username);
         const response = await octokit.request(`GET /users/${username}/repos`, {
             username: 'USERNAME',
             headers: {
@@ -34,19 +33,22 @@ const Github: FC = () => {
     }
 
     return (
-        <main className="w-4/5 my-0 mr-auto	 ml-auto pt-10">
-            <div className="mb-4 flex justify-evenly">
-                <input className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" onChange={(e) => setUserName(e.target.value)}/>
+        <main className="w-4/5 my-0 mr-auto	 ml-auto pt-10 mb-2">
+            <div className="w-1/2 my-0 mx-auto mb-4 flex pl-4">
+                <input className="shadow appearance-none border rounded w-200 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2" id="username" type="text" placeholder="Username" onChange={(e) => setUserName(e.target.value)}/>
                 <button onClick={handleSubmit} className="border border-white-400 px-4 py-2 rounded hover:border-white-900">Go</button>
             </div>
-            {gitData.map((data) => {
-                return (
-                    <li key={data.id} className="flex justify-between">
-                        <p>{data.name}</p>
-                        <a href={data.html_url}>Click here</a>
-                    </li>
-                );
-            })}
+            <div className="w-4/5 my-0 mx-auto pl-10">
+                {gitData.map((data) => {
+                    return (
+                        <li key={data.id} className="flex items-center justify-between w-4/5 border h-12 px-2 mb-2">
+                            <p>{data.name}</p>
+                            <a href={data.html_url}>Click here</a>
+                        </li>
+                    );
+                })}
+            </div>
+            
         </main>
     );
 }
