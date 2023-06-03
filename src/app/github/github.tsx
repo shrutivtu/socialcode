53df2d7d97753d25963eda74b07b0141ab7ae81e
shrutivtu <shrutisaxena1596@gmail.com>
2023-06-03 13:24:04 +0530

    feat: topic feature complete

--- src/app/github/github.tsx
+++ src/app/github/github.tsx
@@ -1,96 +1,119 @@
-"use client"
-import { useState, FC } from "react";
+"use client";
+import { useState, FC, useEffect } from "react";
 import { Octokit } from "octokit";
 import { Topic } from "@/components";
 
-interface GitData {
-    id: number;
-    name: string;
-    html_url: string;
-}
+// interface GitData {
+//   id: number;
+//   name: string;
+//   html_url: string;
+// }
 
 const Github: FC = () => {
-    const [gitData, setGitData] = useState<Array<GitData>>([]);
-    const [username, setUserName] = useState('');
-    const [topicKeyword, setTopicKeyword] = useState('');
-    const [page, setPage] = useState(1);
+  const [gitData, setGitData] = useState<Array<GitData>>([]);
+  const [username, setUserName] = useState("");
+  const [topicKeyword, setTopicKeyword] = useState("");
+  const [page, setPage] = useState(1);
 
-    const octokit = new Octokit({
-        auth: process.env.GIT_KEY
+  useEffect(() => {
+    if(topicKeyword) getTopicResult();
+  }, [page]);;
+
+  const octokit = new Octokit({
+    auth: process.env.GIT_KEY,
+  });
+
+  async function getTest() {
+    const response = await octokit.request(`GET /users/${username}/repos`, {
+      username: "USERNAME",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
     });
+    setGitData(response.data);
+  }
 
-    async function getTest() {
-        const response = await octokit.request(`GET /users/${username}/repos`, {
-            username: 'USERNAME',
-            headers: {
-                "X-GitHub-Api-Version": "2022-11-28",
-            },
+  const getTopicResult = async () => {
+    try {
+      const response = await octokit.request("GET /search/repositories", {
+        q: `topic:${topicKeyword}`,
+        sort: "stars",
+        order: "desc",
+        page: page,
+      });
+
+      // Process the response data
+      const processData = (arr: any) => {
+        const res = arr.map((item: any) => {
+          const {
+            name,
+            html_url,
+            forks_count,
+            topics,
+            visibility,
+            open_issues,
+            watchers,
+          } = item;
+          return {
+            name: name,
+            htmlUrl: html_url,
+            forksCount: forks_count,
+            topics: topics,
+            visibility: visibility,
+            openIssues: open_issues,
+            watchers: watchers,
+          };
         });
-        setGitData(response.data);
+        setGitData(res);
+      };
+      processData(response.data.items);
+    } catch (error) {
+      console.error("Error:", error);
     }
+  };
 
-    const getTopicResult = async () => {
-        try {
-            const response = await octokit.request('GET /search/repositories', {
-                q: `topic:${topicKeyword}`,
-                sort: "stars",
-                order: "desc",
-                page: page
-            });
-        
-            // Process the response data
-            const processData = (arr: any) => {
-                const res = arr.map((item: any) => {
-                    const { name, html_url, forks_count, topics, visibility, open_issues, watchers } = item;
-                    return{
-                        name: name,
-                        htmlUrl: html_url,
-                        forksCount: forks_count,
-                        topics: topics,
-                        visibility: visibility,
-                        openIssues: open_issues,
-                        watchers: watchers
-                    }
-                });
-                setGitData(res);
-            }   
-            processData(response.data.items);
-          } catch (error) {
-            console.error('Error:', error);
-          }
+  const handleSubmit = () => {
+    if (username) {
+      getTest();
     }
-
-    const handleSubmit = () => {
-        if(username){
-            getTest();
-        }
-        if(topicKeyword){
-            getTopicResult();
-        }
+    if (topicKeyword) {
+      getTopicResult();
     }
+  };
 
-    return (
-        <main className="w-4/5 my-0 mr-auto	 ml-auto pt-10 mb-2">
-            <div className="w-9/10 my-0 mx-auto mb-4 flex items-center justify-center relative">
-                <input 
-                    className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2" 
-                    id="username" 
-                    type="text" 
-                    placeholder="Username" 
-                    // onChange={(e) => setUserName(e.target.value)}
-                    onChange={(e) => setTopicKeyword(e.target.value)}
-                />
-                <button onClick={handleSubmit} className="border border-white-400 px-4 py-2 rounded hover:border-white-900 mr-2">Go</button>
-                <button className="border border-white-400 px-4 py-2 rounded hover:border-white-900">+ Add Filter</button>
-                {/* <div className="width-1/5 background-white">
+  const handleChange = (e: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  return (
+    <main className="w-4/5 my-0 mr-auto	 ml-auto pt-10 mb-2">
+      <div className="w-9/10 my-0 mx-auto mb-4 flex items-center justify-center relative">
+        <input
+          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
+          id="username"
+          type="text"
+          placeholder="Username"
+          // onChange={(e) => setUserName(e.target.value)}
+          onChange={(e) => setTopicKeyword(e.target.value)}
+        />
+        <button
+          onClick={handleSubmit}
+          className="border border-white-400 px-4 py-2 rounded hover:border-white-900 mr-2"
+        >
+          Go
+        </button>
+        <button className="border border-white-400 px-4 py-2 rounded hover:border-white-900">
+          + Add Filter
+        </button>
+        {/* <div className="width-1/5 background-white">
                     <ul>
                         <li><input type="checkbox" name="option1" value="option1" />Search By Username</li>
                         <li><input type="checkbox" name="option2" value="option2" />Search By Framework</li>
                     </ul>
                 </div> */}
-            </div>
-            <div className="w-full my-0 mx-auto pl-10 flex flex-col items-center justify-center">
-                {/* {gitData.map((data) => {
+      </div>
+      <div className="w-full my-0 mx-auto pl-10 flex flex-col items-center justify-center">
+        {/* {gitData.map((data) => {
                     return (
                         <li key={data.id} className="flex items-center justify-between w-4/5 border h-12 px-2 mb-2">
                             <p>{data.name}</p>
@@ -98,19 +121,10 @@ const Github: FC = () => {
                         </li>
                     );
                 })} */}
-                {gitData && <Topic gitData={gitData} />}
-            </div>
-        </main>
-    );
-}
+        {gitData && <Topic gitData={gitData} page={page} handleChangePage={handleChange} />}
+      </div>
+    </main>
+  );
+};
 
 export default Github;
-
-
-//name
-//htmlurl
-//forks_count
-//topics: []
-//visibility
-//open_issues
-//watchers
--- src/components/Topic.tsx
+++ src/components/Topic.tsx
@@ -1,42 +1,88 @@
-import React, {FC} from 'react';
+"use client";
+import React, { FC, useState } from "react";
+import Pagination from '@mui/material/Pagination';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '../styles/globalPagination.css';
 
-type ObjectTopic = {
-    name: string,
-    htmlUrl: string,
-    forksCount: number,
-    topics: Array<string>,
-    visibility: string,
-    openIssues: number,
-    watchers: number
+interface GitData {
+    forksCount: number;
+    htmlUrl: string;
+    name: string;
+    openIssues: number;
+    topics: string[];
+    visibility: string;
+    watchers: number;
 }
+  
+type ChildComponentProps = {
+    gitData: GitData[];
+    page: number;
+    handleChangePage: (event: React.ChangeEvent<unknown>, value: number) => {};
 
-type TopicProps = {
-    gitData: Array<ObjectTopic>;
 };
 
-export const Topic: FC<TopicProps> = ({ gitData }) => {
-    console.log(gitData);
+const theme = createTheme({
+    components: {
+      MuiPagination: {
+        styleOverrides: {
+          ul: {
+            '& .MuiPaginationItem-root': {
+              color: '#fff', // Set your desired text color here
+            },
+          },
+        },
+      },
+    //   MuiPaginationItem: {
+    //     styleOverrides: {
+    //       root: {
+    //         backgroundColor: '#3D1D2E', // Set your desired background color here
+    //       },
+    //     },
+    //   },
+    },
+});
+
+export const Topic: FC<ChildComponentProps> = ({ gitData, page, handleChangePage }) => {
   return (
     <>
-    {gitData.map((item, index) => {
-        return(
-        <section className="w-4/5 bg-[#3D1D2E] rounded-md p-3 shadow-2xl mb-4" key={index}>
+      {gitData.map((item, index) => {
+        return (
+          <section
+            className="w-4/5 bg-[#3D1D2E] rounded-md p-3 shadow-2xl mb-4"
+            key={index}
+          >
             <div className="w-full h-10 flex justify-between">
-                <span>{item.name}</span>
-                <span><a href={item.htmlUrl} className="underline-offset-1">Go to the Repo</a></span>
+              <span>{item.name}</span>
+              <span>
+                <a href={item.htmlUrl} className="underline">
+                  Go to the Repository
+                </a>
+              </span>
             </div>
             <div className="w-full h-10 flex justify-between">
-                <span>Forks Count: {item.forksCount}</span>
-                <span>Open Issues: {item.openIssues}</span>
-                <span>Watchers: {item.watchers}</span>
-                <span>Visibility: {item.visibility}</span>
+              <span>Forks Count: {item.forksCount}</span>
+              <span>Open Issues: {item.openIssues}</span>
+              <span>Watchers: {item.watchers}</span>
+              <span>Visibility: {item.visibility}</span>
             </div>
-            <div className="w-full h-10 flex flex-wrap">
-                {item.topics.map((item, index) => <span className="mr-2" key={index}>#{item}</span>)}
+            <div className="w-full flex flex-wrap">
+              {item.topics.map((item, index) => (
+                <span className="mr-2" key={index}>
+                  #{item}
+                </span>
+              ))}
             </div>
-        </section>
-        )
-    })}
+          </section>
+        );
+      })}
+      <ThemeProvider theme={theme}>
+        <Pagination 
+            count={10} 
+            color="secondary" 
+            page={page} 
+            onChange={(e, value) => handleChangePage(e, value)} 
+        />
+      </ThemeProvider>
     </>
-  )
-}
+  );
+};
